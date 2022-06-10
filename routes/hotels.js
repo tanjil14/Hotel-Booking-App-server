3d75eda47fb6d8d2b91e86244248a02a2d12cc2b
tanjil114 <tanjil1@cse.pstu.ac.bd>
2022-06-10 16:58:21 +0600

    adding qurey in getHotels api

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   countByCity,
+  countByType,
   createHotel,
   deleteHotel,
   getHotel,
@@ -11,16 +12,16 @@ import { verifyAdmin } from "../utils/verifyToken.js";
 const router = express.Router();
 
 //CREATE
-router.post("/",verifyAdmin, createHotel);
+router.post("/", verifyAdmin, createHotel);
 //UPDATE
-router.put("/:id",verifyAdmin, updateHotel);
+router.put("/:id", verifyAdmin, updateHotel);
 //DELETE
-router.delete("/:id",verifyAdmin, deleteHotel);
+router.delete("/:id", verifyAdmin, deleteHotel);
 //GET
 router.get("/find/:id", getHotel);
 //GET All
 router.get("/", getHotels);
 router.get("/countByCity", countByCity);
-router.get("/countByType", getHotels);
+router.get("/countByType", countByType);
 
 export default router;
